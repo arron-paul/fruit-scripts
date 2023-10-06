cac2bd23ca2ee417cf7e9a1d4b5f10be10428638
Arron <hello@arron.io>
2023-10-06 15:34:24 +0100

    Convert tab to spaces and fix bug in Scheduler

--- Scheduled/Iconography/Update-AppIcons.zsh
+++ Scheduled/Iconography/Update-AppIcons.zsh
@@ -14,55 +14,55 @@ declare -a app_bundles=("${(@f)$(/usr/bin/mdfind "kMDItemContentType == 'com.app
 
 # Function to test if an app has a custom icon
 function test_custom_icon {
-	local app=$1
-	$(brew --prefix)/bin/fileicon test "$app" &> /dev/null
-	local fileicon_exit_code=$?
-	return $fileicon_exit_code
+  local app=$1
+  $(brew --prefix)/bin/fileicon test "$app" &> /dev/null
+  local fileicon_exit_code=$?
+  return $fileicon_exit_code
 }
 
 # Function to apply a custom icon to an app
 function apply_custom_icon {
-	local app=$1
-	local icon=$2
-	$(brew --prefix)/bin/fileicon set "$app" "$icon" &> /dev/null
-	local fileicon_exit_code=$?
-	return $fileicon_exit_code
+  local app=$1
+  local icon=$2
+  $(brew --prefix)/bin/fileicon set "$app" "$icon" &> /dev/null
+  local fileicon_exit_code=$?
+  return $fileicon_exit_code
 }
 
 # Function to remove a custom icon from an app
 function remove_custom_icon {
-	local app=$1
-	$(brew --prefix)/bin/fileicon rm "$app" &> /dev/null
-	local fileicon_exit_code=$?
-	return $fileicon_exit_code
+  local app=$1
+  $(brew --prefix)/bin/fileicon rm "$app" &> /dev/null
+  local fileicon_exit_code=$?
+  return $fileicon_exit_code
 }
 
 # Iterate through app bundles
 for app in "${app_bundles[@]}"; do
 
-	# Check if bundle has a custom icon
-	if test_custom_icon "$app"; then
+  # Check if bundle has a custom icon
+  if test_custom_icon "$app"; then
 
-		# If a custom icon has already been applied and
-		# `ENABLE_APP_ICONS` is disabled, remove the icon
-		if [ "$ENABLE_APP_ICONS" = "false" ]; then
-			remove_custom_icon "$app"
-		fi
+    # If a custom icon has already been applied and
+    # `ENABLE_APP_ICONS` is disabled, remove the icon
+    if [ "$ENABLE_APP_ICONS" = "false" ]; then
+      remove_custom_icon "$app"
+    fi
 
-	else
+  else
 
-		# Apply custom icon if feature enabled
-		if [ "$ENABLE_APP_ICONS" = "true" ]; then
-			declare bundle_name=$(basename "$app" | cut -f 1 -d '.')
-			# Check for icons in custom icon directory
-			if [ -f "$CUSTOM_ICONS_DIR/$bundle_name.png" ]; then
-				apply_custom_icon "$app" "$CUSTOM_ICONS_DIR/$bundle_name.png"
-			elif [ -f "$CUSTOM_ICONS_DIR/$bundle_name.icns" ]; then
-				apply_custom_icon "$app" "$CUSTOM_ICONS_DIR/$bundle_name.icns"
-			fi
-		fi
+    # Apply custom icon if feature enabled
+    if [ "$ENABLE_APP_ICONS" = "true" ]; then
+      declare bundle_name=$(basename "$app" | cut -f 1 -d '.')
+      # Check for icons in custom icon directory
+      if [ -f "$CUSTOM_ICONS_DIR/$bundle_name.png" ]; then
+        apply_custom_icon "$app" "$CUSTOM_ICONS_DIR/$bundle_name.png"
+      elif [ -f "$CUSTOM_ICONS_DIR/$bundle_name.icns" ]; then
+        apply_custom_icon "$app" "$CUSTOM_ICONS_DIR/$bundle_name.icns"
+      fi
+    fi
 
-	fi
+  fi
 done
 
 exit 0
